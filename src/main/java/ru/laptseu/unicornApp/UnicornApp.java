39eabfa41f0416ae125723e30703991149070507
kleshnya-1 <52631250+kleshnya-1@users.noreply.github.com>
2021-12-03 08:11:53 +0300

    Delete UnicornApp.java

--- src/main/java/ru/laptseu/unicornApp/UnicornApp.java
+++
@@ -1,9 +0,0 @@
-package ru.laptseu.unicornApp;
-
-import org.springframework.boot.SpringApplication;
-
-public class UnicornApp {
-    public static void main(String[] args) {
-        SpringApplication.run(UnicornApp.class, args);
-    }
-}
