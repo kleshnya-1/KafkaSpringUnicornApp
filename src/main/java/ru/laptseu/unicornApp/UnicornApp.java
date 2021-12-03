7ca88f567fcac44b487d45ddb04ff7d85f1dc346
Andrey <andrei753852@gmail.com>
2021-12-03 08:12:09 +0300

    0

---
+++ src/main/java/ru/laptseu/unicornApp/UnicornApp.java
@@ -0,0 +1,9 @@
+package ru.laptseu.unicornApp;
+
+import org.springframework.boot.SpringApplication;
+
+public class UnicornApp {
+    public static void main(String[] args) {
+        SpringApplication.run(UnicornApp.class, args);
+    }
+}
